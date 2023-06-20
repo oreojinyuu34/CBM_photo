a605d0bcd723dd96de41d885d5021986e492510c
atsushi <77239082+oreojinyuu34@users.noreply.github.com>
2023-06-21 07:37:05 +0900

    first commit

---
+++ public/js/main.js
@@ -0,0 +1,6 @@
+function revealImage() {
+  var chekiImage = document.getElementById("cheki-image");
+  var chekiFrame = document.getElementById("cheki-frame");
+  chekiImage.classList.add("revealed");
+  chekiFrame.classList.add("revealed");
+}
